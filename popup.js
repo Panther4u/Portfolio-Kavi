93dfaffad8e7aa6fef374c90f5c6d4c0a5cbead9
Kavin2429 <kavinkaviya7@gmail.com>
2023-12-01 03:42:48 -0800

    adjustment

--- popup.js
+++
@@ -1,9 +0,0 @@
-var openBtn = document.querySelector("open-btn");
-var closeBtn = document.querySelector("close-btn");
-
-openBtn.addEventListener("click",function(){
-    document.body.classList.add("popup-active")
-});
-closeBtn.addEventListener("click",function(){
-    document.body.classList.remove("popup-active")
-});
